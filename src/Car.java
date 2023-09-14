57867c50272a6996819db71129adf9d784621485
Eugene <zparkhuts@gmail.com>
2023-09-14 14:30:41 +0300

    Created Car and Customer classes

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Car {
+    private String make;
+    private String brand;
+    private int year;
+    private int price;
+    private int id;
+
+    Car(String make,String brand,int year,int price,int id){
+        this.make = make;
+        this.brand = brand;
+        this.year = year;
+        this.price = price;
+        this.id = id;
+    }
+
+
+
+}
---
+++ src/Customer.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private String phoneNumber;
+    private ArrayList<Car> listOfBoughtCars;
+
+    Customer(String name,String phoneNumber){
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
