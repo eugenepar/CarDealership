d8796298ae5b608c3693348f6cf284a5fc850d14
Eugene <zparkhuts@gmail.com>
2023-09-18 14:52:04 +0300

    Updated the Dealership Class

--- src/CarDealership.java
+++ src/CarDealership.java
@@ -3,16 +3,30 @@ import java.util.ArrayList;
 public class CarDealership {
     private ArrayList<Car> carsForSale = new ArrayList<>();
 
-    public void addCar(Car car){
+    public void addCar(Car car) {
         carsForSale.add(car);
     }
-    public Boolean sellCar(Car car,Customer customer){
-        if (carsForSale.remove(car)){
+
+    public Boolean sellCar(Car car, Customer customer) {
+        if (carsForSale.remove(car)) {
             customer.buyCar(car);
             return true;
         }
         return false;
     }
 
+    public ArrayList<Car> getCarsForSale() {
+        return this.carsForSale;
+    }
+
+    public Boolean returnCar(Car car, Customer customer) {
+        if (customer.returnCar(car)) {
+            carsForSale.add(car);
+            return true;
+        }
+        return false;
+    }
 }
+
+
 
